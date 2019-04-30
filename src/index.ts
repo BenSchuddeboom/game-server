5e00e15e1ba81c493fa6ddf21b61f627b79ce977
BenSchuddeboom <Benzel@live.nl>
2019-04-30 15:45:55 +0200

    added a players object

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ const socket = require('socket.io')
 const port = process.env.PORT || 4000
 
 const app = express()
-const server = app.listen(4000, () => {
-    console.log('Server ready and listening on port 4000.')
+const server = app.listen(port, () => {
+    console.log('Server ready and listening on port ' + port)
 })
 const io = socket(server)
 
@@ -14,6 +14,25 @@ app.use(
     bodyparser.json()
 )
 
+const players = {}
+
 io.on('connection', (socket) => {
     console.log(`User [${socket.id}] connected. `)
+
+    socket.on('disconnect', () => {
+        console.log(`User [${socket.id}] disconnected. `)
+        delete players[socket.id]
+        io.emit('disconnect', socket.id)
+    })
+
+    players[socket.id] = {
+        rotation: 0,
+        x: Math.floor(Math.random() * 700) + 50,
+        y: Math.floor(Math.random() * 500) + 50,
+        playerId: socket.id
+    }
+
+    socket.emit('currentPlayers', players)
+    socket.broadcast.emit('newPlayer', players[socket.id])
+    console.log(players[socket.id])
 })
