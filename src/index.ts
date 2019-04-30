93a133ac79652453a88945412c1a8ebc7d8d9f82
BenSchuddeboom <Benzel@live.nl>
2019-04-30 23:05:46 +0200

    add server code to provide momement updates to client

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,11 @@ const socket = require('socket.io')
 const port = process.env.PORT || 4000
 
 const app = express()
+
 const server = app.listen(port, () => {
     console.log('Server ready and listening on port ' + port)
 })
+
 const io = socket(server)
 
 app.use(
@@ -17,22 +19,28 @@ app.use(
 const players = {}
 
 io.on('connection', (socket) => {
-    console.log(`User [${socket.id}] connected. `)
+        console.log(`User [${socket.id}] connected. `)
 
-    socket.on('disconnect', () => {
-        console.log(`User [${socket.id}] disconnected. `)
-        delete players[socket.id]
-        io.emit('disconnect', socket.id)
-    })
+        players[socket.id] = {
+            x: Math.floor(Math.random() * 700) + 50,
+            y: Math.floor(Math.random() * 500) + 50,
+            playerId: socket.id
+        }
 
-    players[socket.id] = {
-        rotation: 0,
-        x: Math.floor(Math.random() * 700) + 50,
-        y: Math.floor(Math.random() * 500) + 50,
-        playerId: socket.id
-    }
+        socket.emit('currentPlayers', players)
+        socket.broadcast.emit('newPlayer', players[socket.id])
+        console.log(players)
 
-    socket.emit('currentPlayers', players)
-    socket.broadcast.emit('newPlayer', players[socket.id])
-    console.log(players[socket.id])
+        socket.on('disconnect', () => {
+            console.log(`User [${socket.id}] disconnected. `)
+            delete players[socket.id]
+            io.emit('disconnect', socket.id)
+        })
+
+    socket.on('playerMovement', (movementData) => {
+        players[socket.id].x = movementData.x;
+        players[socket.id].y = movementData.y;
+
+        socket.broadcast.emit('playerMoved', players[socket.id]);
+    });
 })
