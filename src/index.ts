0f19189c941cadea187f16d8e7444bb638397702
Albert <albert@diskus.studio>
2019-05-02 16:22:31 +0200

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
-const express = require('express')
-const bodyparser = require('body-parser')
-const socket = require('socket.io')
+import "reflect-metadata";
+import { createExpressServer } from "routing-controllers";
+import setupDb from './db'
+import ScoreboardController from './entity/controller'
 
-const port = process.env.PORT || 4000
+const app = createExpressServer({
+    controllers: [
+        ScoreboardController
+    ]
+ });
+ 
+const bodyparser = require('body-parser');
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
-const app = express()
+const expressPort = process.env.EX_PORT || 4000
+const databasePort = process.env.DB_PORT || 5000
 
-const server = app.listen(port, () => {
-    console.log('Server ready and listening on port ' + port)
-})
+server.listen(expressPort, console.log('server running.'))
 
-const io = socket(server)
 
 app.use(
     bodyparser.json()
@@ -18,18 +25,26 @@ app.use(
 
 const players = {}
 
+const cookie = {
+  x: Math.floor(Math.random() * 700) + 50,
+  y: Math.floor(Math.random() * 500) + 50
+};
+
 io.on('connection', (socket) => {
         console.log(`User [${socket.id}] connected. `)
 
         players[socket.id] = {
             x: Math.floor(Math.random() * 700) + 50,
             y: Math.floor(Math.random() * 500) + 50,
-            playerId: socket.id
+            playerId: socket.id,
+            score: 0,
+            name: null
         }
 
         socket.emit('currentPlayers', players)
         socket.broadcast.emit('newPlayer', players[socket.id])
-        console.log(players)
+        socket.emit('spawnCookie', cookie);
+        //console.log(players)
 
         socket.on('disconnect', () => {
             console.log(`User [${socket.id}] disconnected. `)
@@ -43,4 +58,21 @@ io.on('connection', (socket) => {
 
         socket.broadcast.emit('playerMoved', players[socket.id]);
     });
+
+    socket.on('cookieCollected', function () {
+        players[socket.id].score += 1
+        cookie.x = Math.floor(Math.random() * 700) + 50;
+        cookie.y = Math.floor(Math.random() * 500) + 50;
+        io.emit('spawnCookie', cookie);
+        //not sure if we need this scoreupdate emit, but hey.
+        io.emit('scoreUpdate', players[socket.id]);
+        console.log(players)
+    });
 })
+
+setupDb()
+  .then(_ => {
+    io.listen(databasePort)
+    console.log(`Listening on port ${databasePort}`)
+  })
+  .catch(err => console.error(err))
